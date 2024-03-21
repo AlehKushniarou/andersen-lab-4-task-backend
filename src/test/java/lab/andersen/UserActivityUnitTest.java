f94480f1402250a140186bb8608b3a6138e830ee
Maksim Shevchuk <maskim27012003@gmail.com>
2024-03-21 19:15:43 +0300

    some updated tests

--- src/test/java/lab/andersen/UserActivityUnitTest.java
+++ src/test/java/lab/andersen/UserActivityUnitTest.java
@@ -1,9 +1,13 @@
 package lab.andersen;
 
 import lab.andersen.dao.UserActivityDao;
+import lab.andersen.dto.CreateUserActivityDto;
+import lab.andersen.dto.UserActivityDto;
+import lab.andersen.dto.UserActivityShortDto;
+import lab.andersen.entity.UserActivity;
 import lab.andersen.exception.DaoException;
 import lab.andersen.exception.ServiceException;
-//import lab.andersen.model.UserActivity;
+import lab.andersen.service.UserActivityService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -13,6 +17,7 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -20,52 +25,72 @@ import static org.mockito.Mockito.*;
 
 public class UserActivityUnitTest {
 
-//    @Mock
-//    private UserActivityDao dao;
-//
-//    @InjectMocks
-//    private UserActivityServiceImpl service;
-//
-//    public UserActivityUnitTest() {
-//        MockitoAnnotations.openMocks(this);
-//    }
-//
-//    @Test
-//    public void findAllUsersActivities() throws Exception {
-//        List<UserActivity> expectedActivities = new ArrayList<>();
-//        expectedActivities.add(new UserActivity(1, 1, "test1", Timestamp.valueOf("2024-01-01 09:01:15")));
-//        expectedActivities.add(new UserActivity(2, 1, "test2", Timestamp.valueOf("2024-01-01 09:01:15")));
-//        when(dao.findAll()).thenReturn(expectedActivities);
-//        List<UserActivity> actualActivities = service.findAllUsersActivities();
-//        verify(dao, times(1)).findAll();
-//        assertEquals(expectedActivities.size(), actualActivities.size());
-//    }
-//
-//    @Test
-//    public void findAllUsersActivitiesThrowsException() throws Exception {
-//        when(dao.findAll()).thenThrow(DaoException.class);
-//        assertThrows(ServiceException.class, () -> service.findAllUsersActivities());
-//        verify(dao, times(1)).findAll();
-//    }
-//
-//    @Test
-//    public void findAllTodayActivities() throws Exception {
-//        Date date = new Date();
-//        List<UserActivity> expectedActivities = new ArrayList<>();
-//        expectedActivities.add(new UserActivity(1, 1, "test1", new Timestamp(date.getTime())));
-//        expectedActivities.add(new UserActivity(2, 1, "test2", new Timestamp(date.getTime())));
-//        when(dao.findAll()).thenReturn(expectedActivities);
-//        List<UserActivity> actualActivities = service.findAllUsersActivities();
-//        verify(dao, times(1)).findAll();
-//        assertEquals(expectedActivities.size(), actualActivities.size());
-//    }
-//
-//    @Test
-//    public void findAllTodayActivitiesThrowsException() throws Exception {
-//        when(dao.findAll()).thenThrow(DaoException.class);
-//        assertThrows(ServiceException.class, () -> service.findAllUsersActivities());
-//        verify(dao, times(1)).findAll();
-//    }
+    @Mock
+    private UserActivityDao dao;
+
+    @InjectMocks
+    private UserActivityService service;
+
+    public UserActivityUnitTest() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    public void findAllTodayActivities() throws Exception {
+        Date date = new Date();
+        List<UserActivityShortDto> expectedActivities = new ArrayList<>();
+        expectedActivities.add(new UserActivityShortDto(1, "username1", "test1", new Timestamp(date.getTime())));
+        expectedActivities.add(new UserActivityShortDto(1, "username2", "test2", new Timestamp(date.getTime())));
+        when(dao.findAllToday()).thenReturn(expectedActivities);
+        List<UserActivityShortDto> actual = service.findAllTodayActivities();
+        assertEquals(expectedActivities, actual);
+        verify(dao, times(1)).findAllToday();
+    }
+
+    @Test
+    public void findAllTodayActivitiesThrowsException() throws Exception {
+        when(dao.findAllToday()).thenThrow(DaoException.class);
+        assertThrows(ServiceException.class, () -> service.findAllTodayActivities());
+        verify(dao, times(1)).findAllToday();
+    }
+
+    @Test
+    public void findById() throws Exception {
+        UserActivity expectedUserActivity = new UserActivity(1, 1, "test1", Timestamp.valueOf("2024-01-01 01:01:01"));
+        UserActivityDto expectedDto = new UserActivityDto(expectedUserActivity.getUserId(), expectedUserActivity.getDescription(), expectedUserActivity.getDateTime());
+        when(dao.findById(expectedUserActivity.getId())).thenReturn(Optional.ofNullable(expectedUserActivity));
+        UserActivityDto actualDto = service.findById(expectedUserActivity.getId());
+        assertEquals(expectedDto, actualDto);
+        verify(dao, times(1)).findById(expectedUserActivity.getId());
+    }
+
+    @Test
+    public void findByIdThrowsException() throws Exception {
+        when(dao.findById(anyInt())).thenThrow(DaoException.class);
+        assertThrows(ServiceException.class, () -> service.findById(anyInt()));
+        verify(dao, times(1)).findById(anyInt());
+    }
+
+    @Test
+    public void create() throws Exception {
+        int numberOfRowsAffected = 1;
+        String name = "test1";
+        CreateUserActivityDto dto = new CreateUserActivityDto("description1");
+        when(dao.create(name, dto.getDescription())).thenReturn(numberOfRowsAffected);
+        assertEquals(numberOfRowsAffected, service.create(name, dto));
+        verify(dao, times(1)).create(name, dto.getDescription());
+    }
+
+    @Test
+    public void createThrowsException() throws Exception {
+        String name = "test1";
+        CreateUserActivityDto dto = new CreateUserActivityDto("description1");
+        when(dao.create(name, dto.getDescription())).thenThrow(DaoException.class);
+        assertThrows(ServiceException.class, ()->service.create(name, dto));
+        verify(dao, times(1)).create(anyString(), anyString());
+    }
+
 //
 //    @Test
 //    public void updateUserActivity() throws Exception {
